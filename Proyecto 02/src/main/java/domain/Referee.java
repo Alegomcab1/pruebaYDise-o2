6d93cf6221df12b7c82631b8c7bb90fa9b73d96e
daniarcaa <danielnf@hotmail.es>
2018-10-29 13:58:58 +0100

    Modificacion referee

--- Proyecto 02/src/main/java/domain/Referee.java
+++ Proyecto 02/src/main/java/domain/Referee.java
@@ -1,6 +1,23 @@
 
 package domain;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 public class Referee extends Actor {
+
+	private List<Report>	reports;
+
+
+	@Valid
+	public List<Report> getReports() {
+		return this.reports;
+	}
+
+	@Valid
+	public void setReports(final List<Report> reports) {
+		this.reports = reports;
+	}
 
 }
