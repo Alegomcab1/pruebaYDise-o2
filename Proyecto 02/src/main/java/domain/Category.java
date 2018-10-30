0bc822440539f96ca7923fae2a97cbc867309235
Alegomcab1 <alegomcab1@gmail.com>
2018-10-30 11:31:59 +0100

    Section, Category y Phase

---
+++ Proyecto 02/src/main/java/domain/Category.java
@@ -0,0 +1,32 @@
+
+package domain;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Category extends DomainEntity {
+
+	private List<Category>	subCategorys;
+	private String			name;
+
+
+	@Valid
+	public List<Category> getSubCategorys() {
+		return this.subCategorys;
+	}
+	@Valid
+	public void setSubCategorys(final List<Category> subCategorys) {
+		this.subCategorys = subCategorys;
+	}
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+	@NotBlank
+	public void setName(final String name) {
+		this.name = name;
+	}
+}
--- Proyecto 02/src/main/java/domain/Section.java
+++ Proyecto 02/src/main/java/domain/Section.java
@@ -3,35 +3,22 @@ package domain;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import javax.persistence.Entity;
-=======
-import javax.validation.constraints.Min;
->>>>>>> 0cb384a603f914f79f7964aeb6270b511b5f2d53
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
-<<<<<<< HEAD
 @Entity
 public class Section {
 
 	private String			sectionTitle;
 	private String			text;
 	private List<String>	sectionPictures;
-=======
-public class Section extends DomainEntity {
-
-	private String			title;
-	private String			text;
-	private List<String>	pictures;
->>>>>>> 0cb384a603f914f79f7964aeb6270b511b5f2d53
 	private int				number;
 
 
 	@NotBlank
-<<<<<<< HEAD
 	public String getSectionTitle() {
 		return this.sectionTitle;
 	}
@@ -39,15 +26,6 @@ public class Section extends DomainEntity {
 	@NotBlank
 	public void setSectionTitle(final String sectionTitle) {
 		this.sectionTitle = sectionTitle;
-=======
-	public String getTitle() {
-		return this.title;
-	}
-
-	@NotBlank
-	public void setTitle(final String title) {
-		this.title = title;
->>>>>>> 0cb384a603f914f79f7964aeb6270b511b5f2d53
 	}
 
 	@NotBlank
@@ -61,7 +39,6 @@ public class Section extends DomainEntity {
 	}
 
 	@URL
-<<<<<<< HEAD
 	public List<String> getSectionPictures() {
 		return this.sectionPictures;
 	}
@@ -72,28 +49,11 @@ public class Section extends DomainEntity {
 	}
 
 	@NotNull
-=======
-	public List<String> getPictures() {
-		return this.pictures;
-	}
-
-	@URL
-	public void setPictures(final List<String> pictures) {
-		this.pictures = pictures;
-	}
-
-	@NotNull
-	@Min(0)
->>>>>>> 0cb384a603f914f79f7964aeb6270b511b5f2d53
 	public int getNumber() {
 		return this.number;
 	}
 
 	@NotNull
-<<<<<<< HEAD
-=======
-	@Min(0)
->>>>>>> 0cb384a603f914f79f7964aeb6270b511b5f2d53
 	public void setNumber(final int number) {
 		this.number = number;
 	}
